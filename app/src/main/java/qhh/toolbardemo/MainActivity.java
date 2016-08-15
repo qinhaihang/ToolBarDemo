8c5ec876e24ee774b13d5bf993ef466e043a7b90
qinghaihang <hai449769034@qq.com>
2016-08-15 16:11:39 +0800

    ToolBar小demo

---
+++ app/src/main/java/qhh/toolbardemo/MainActivity.java
@@ -0,0 +1,69 @@
+package qhh.toolbardemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d(TAG,"onCreate");
+
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.mipmap.icon_list);
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.tb_search:
+                        Toast.makeText(MainActivity.this,"tb_search",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.tb_msg:
+                        Toast.makeText(MainActivity.this,"tb_msg",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        Log.d(TAG,"onCreateOptionsMenu");
+
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+
+        return true;
+    }
+
+}
