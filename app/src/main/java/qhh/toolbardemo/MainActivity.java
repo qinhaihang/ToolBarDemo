5c03d78576e315186c25363073fdde2efd34b9ff
qinghaihang <hai449769034@qq.com>
2016-08-15 17:57:01 +0800

    添加 navigation的时候 出错，原因未明

--- app/src/main/java/qhh/toolbardemo/MainActivity.java
+++ app/src/main/java/qhh/toolbardemo/MainActivity.java
@@ -17,10 +17,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         Log.d(TAG,"onCreate");
-
-        setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         mToolbar.setNavigationIcon(R.mipmap.icon_list);
